67e6bc649f89a6891f06b9ef5d1fd588639fadfb
ILYA <aleksin095@gmail.com>
2022-01-22 13:14:05 +0300

    hw1

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const http = require("http")
+const fs = require("fs")
 
 const host = "localhost"
 const port = 8000
 
 const requestListener = (req, res) => {
-    res.writeHead(200)
-    res.end(`Hello! It's my first server`)
+    if (req.url === "/get" && req.method === "GET") {
+        try {
+            let files = fs.readdirSync("files")
+            res.writeHead(200)
+            res.end(files.join(", "))
+        } catch (err) {
+            res.writeHead(500)
+            res.end("Internal server error")
+        }
+    } else if (req.url === "/delete" && req.method === "DELETE") {
+        res.writeHead(200)
+        res.end("Success")
+    } else if (req.url === "/post" && req.method === "POST") {
+        res.writeHead(200)
+        res.end("Success")
+    } else if (req.url === "/redirect" && req.method === "GET") {
+        res.writeHead(307, {
+            Location: "/redirected",
+        }).end()
+    } else if (req.url === "/redirected" && req.method === "GET") {
+        res.writeHead(200)
+        res.end("Successful redirection")
+    } else {
+        res.writeHead(405)
+        res.end("HTTP method not allowed")
+    }
 }
 
 const server = http.createServer(requestListener)
